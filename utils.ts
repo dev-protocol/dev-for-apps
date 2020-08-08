23a7b522120e77e876676b8663ec635b0ae74e2f
Hideo Hattori <hhatto.jp@gmail.com>
2020-08-08 23:52:54 +0900

    ignore lint warning

--- utils.ts
+++ utils.ts
@@ -1,5 +1,6 @@
 import { Context } from '@azure/functions'
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const responseCreator = (context: Context) => (
 	status = 200,
 	body: string | Record<string, unknown> = ''
