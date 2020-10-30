aa37729b074bbed10c657e6d037133724438385a
aggre <hiroyuki.aggre@gmail.com>
2020-10-30 10:12:46 +0900

    Development flow tweaks (#71)
    * add lint and formatter
    
    * apply the lint
    
    * add strapi building files to prettierignore
    
    * add github actions
    
    * automerge

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '514444c21c14702665bb1554b45f99dc'),
-    },
-  },
-});
+	host: env('HOST', '0.0.0.0'),
+	port: env.int('PORT', 1337),
+	admin: {
+		auth: {
+			secret: env('ADMIN_JWT_SECRET', '514444c21c14702665bb1554b45f99dc'),
+		},
+	},
+})
